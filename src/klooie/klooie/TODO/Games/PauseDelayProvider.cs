7b9052ba839dc41135f374c2fa2b56e89f9dff30
Adam Abdelhamed <adamabdelhamed@live.com>
2022-09-08 11:32:30 -0400

    fix ms bug

--- src/klooie/klooie/TODO/Games/PauseDelayProvider.cs
+++ src/klooie/klooie/TODO/Games/PauseDelayProvider.cs
@@ -5,7 +5,7 @@ internal class PauseDelayProvider : IDelayProvider
     public PauseDelayProvider(PauseManager manager) => this.manager = manager;
     public Task DelayFuzzyAsync(float ms, double maxDeltaPercentage = 0.1) => Delay(ms);
     public Task Delay(double ms) => manager.Delay((float)ms);
-    public Task Delay(TimeSpan timeout) => manager.Delay(timeout.Milliseconds);
+    public Task Delay(TimeSpan timeout) => manager.Delay(timeout.TotalMilliseconds);
  
 
     private async Task Yield() => await Task.Yield();
