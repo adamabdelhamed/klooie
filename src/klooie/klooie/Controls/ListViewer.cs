156808f66df408d9a23d1719a3803375b64c9116
Adam Abdelhamed <adamabdelhamed@live.com>
2022-10-18 10:47:22 -0400

    Grid reconciliation

---
+++ src/klooie/klooie/Controls/ListViewer.cs
@@ -0,0 +1,481 @@
+﻿namespace klooie;
+
+public class ListViewerOptions<T> where T : class
+{
+    public bool ShowColumnHeaders { get; set; } = true;
+    public bool ShowPager { get; set; } = true;
+    public bool EnablePagerKeyboardShortcuts { get; set; } = true;
+    public ListViewerSelectionMode SelectionMode { get; set; } = ListViewerSelectionMode.Row;
+    public List<HeaderDefinition<T>> Columns { get; set; }
+    public IList<T> DataSource { get; set; }
+}
+
+public enum ListViewerSelectionMode
+{
+    None,
+    Row,
+    Cell
+}
+
+public class HeaderDefinition : GridColumnDefinition
+{
+    public ConsoleString Header { get; set; }
+
+}
+
+public class HeaderDefinition<T> : HeaderDefinition where T : class
+{ 
+    public Func<T, ConsoleControl> Formatter { get; set; }
+}
+
+public class ListViewer<T> : ProtectedConsolePanel where T : class
+{
+    private ListViewerOptions<T> options;
+    private int lastTopOfPageIndex;
+    private int topOfPageDataIndex = 0;
+
+    private ListViewerPanel presenter;
+    private List<ConsoleControl> highlightedControls;
+    private List<Lifetime> highlightLifetimes = new List<Lifetime>();
+
+    public Event SelectionChanged { get; private init; } = new Event();
+    public int SelectedRowIndex { get => Get<int>(); set => Set(value); }
+    public int SelectedColumnIndex { get => Get<int>(); set => Set(value); }
+    public int PageIndex => (int)Math.Floor(topOfPageDataIndex / (double)presenter.MaxRowsThatCanBePresented);
+    public int PageCount => (int)Math.Ceiling(options.DataSource.Count / (double)presenter.MaxRowsThatCanBePresented);
+
+    public ListViewer(ListViewerOptions<T> options)
+    {
+        this.options = options;
+        highlightedControls = new List<ConsoleControl>();
+        CanFocus = options.SelectionMode != ListViewerSelectionMode.None;
+        Focused.Subscribe(UpdateHighlightedRowsToReflectCurrentFocus, this);
+        Unfocused.Subscribe(UpdateHighlightedRowsToReflectCurrentFocus, this);
+        KeyInputReceived.Subscribe(HandleArrows, this);
+
+        presenter = ProtectedPanel.Add(new ListViewerPanel(new ListViewerPanelOptions()
+        {
+            Columns = options.Columns.Cast<HeaderDefinition>().ToList(),
+            ShowColumnHeaders = options.ShowColumnHeaders,
+            ShowPager = options.ShowPager,
+            EnablePagerKeyboardShortcuts = options.EnablePagerKeyboardShortcuts,
+        })).Fill();
+
+        presenter.BeforeRecompose.Subscribe(BeforeRecompose, this);
+        presenter.AfterRecompose.Subscribe(UpdateHighlightedRowsToReflectCurrentFocus, this);
+        presenter.FirstPageClicked.Subscribe(FirstPageClicked, this);
+        presenter.PreviousPageClicked.Subscribe(PreviousPageClicked, this);
+        presenter.NextPageClicked.Subscribe(NextPageClicked, this);
+        presenter.LastPageClicked.Subscribe(LastPageClicked, this);
+        this.Subscribe(nameof(SelectedRowIndex), SelectedRowChanged, this);
+        this.Subscribe(nameof(SelectedColumnIndex), SelectedColumnChanged, this);
+    }
+
+    /// <summary>
+    /// We could just call presenter.Recompose() when the selected row changes, but it would be slow.
+    /// Instead, we only do this if this selection change caused a page change. In the cases when the 
+    /// selection change is within the current page we will just change the highlighted controls, which
+    /// is much more performant.
+    /// </summary>
+    private void SelectedRowChanged()
+    {
+        if (lastTopOfPageIndex != topOfPageDataIndex)
+        {
+            presenter.Recompose();
+        }
+        else
+        {
+            var presentedRowIndex = SelectedRowIndex - topOfPageDataIndex;
+            var rowControls = presenter.ControlsByRow[presentedRowIndex];
+            highlightedControls.Clear();
+
+            for (var i = 0; i < rowControls.Count; i++)
+            {
+                if (options.SelectionMode == ListViewerSelectionMode.Row || i == SelectedColumnIndex)
+                {
+                    highlightedControls.Add(rowControls[i]);
+                }
+            }
+
+            Highlight(highlightedControls);
+        }
+        lastTopOfPageIndex = topOfPageDataIndex;
+        SelectionChanged.Fire();
+    }
+
+    private void SelectedColumnChanged()
+    {
+        var rowControls = presenter.ControlsByRow[SelectedRowIndex - topOfPageDataIndex];
+        highlightedControls.Clear();
+
+        for (var i = 0; i < rowControls.Count; i++)
+        {
+            if (options.SelectionMode == ListViewerSelectionMode.Row || i == SelectedColumnIndex)
+            {
+                highlightedControls.Add(rowControls[i]);
+            }
+        }
+
+        Highlight(highlightedControls);
+        SelectionChanged.Fire();
+    }
+
+    public void Refresh() => presenter.Recompose();
+    
+
+
+    private void BeforeRecompose()
+    {
+        highlightedControls.Clear();
+        // ensure the top of the page is on a proper page boundary
+        while (topOfPageDataIndex % presenter.MaxRowsThatCanBePresented != 0)
+        {
+            topOfPageDataIndex--;
+        }
+
+        if (options.SelectionMode != ListViewerSelectionMode.None)
+        {
+            // ensure that the selected row is in the viewport
+            while (SelectedRowIndex < topOfPageDataIndex)
+            {
+                topOfPageDataIndex -= presenter.MaxRowsThatCanBePresented;
+            }
+
+            while (SelectedRowIndex >= topOfPageDataIndex + presenter.MaxRowsThatCanBePresented)
+            {
+                topOfPageDataIndex += presenter.MaxRowsThatCanBePresented;
+            }
+        }
+
+        var range = options.DataSource.Skip(topOfPageDataIndex).Take(presenter.MaxRowsThatCanBePresented).ToArray();
+        presenter.Options.Rows = new List<DataGridPresentationRow>();
+
+        for (var i = 0; i < range.Length; i++)
+        {
+            var item = range[i];
+            var deepIndex = i + topOfPageDataIndex;
+            var row = new DataGridPresentationRow();
+            presenter.Options.Rows.Add(row);
+            for (var j = 0; j < options.Columns.Count; j++)
+            {
+                var col = options.Columns[j];
+
+                bool shouldBeHighlighted = false;
+
+                if (options.SelectionMode == ListViewerSelectionMode.Row && deepIndex == SelectedRowIndex)
+                {
+                    shouldBeHighlighted = true;
+                }
+                else if (options.SelectionMode == ListViewerSelectionMode.Cell && deepIndex == SelectedRowIndex && SelectedColumnIndex == j)
+                {
+                    shouldBeHighlighted = true;
+                }
+
+                row.Cells.Add(() =>
+                {
+                    var control = col.Formatter(item);
+                    if (shouldBeHighlighted)
+                    {
+                        highlightedControls.Add(control);
+                    }
+                    return control;
+                });
+            }
+        }
+
+        presenter.Options.PagerState = new PagerState()
+        {
+            AllowRandomAccess = true,
+            CanGoBackwards = PageIndex > 0,
+            CanGoForwards = PageIndex < PageCount - 1,
+            CurrentPageLabelValue = $"Page {PageIndex + 1} of {PageCount}".ToConsoleString(),
+        };
+    }
+
+    private void UpdateHighlightedRowsToReflectCurrentFocus() => Highlight(highlightedControls);
+
+    private void Highlight(List<ConsoleControl> controls)
+    {
+        foreach (var lifetime in highlightLifetimes)
+        {
+            lifetime.Dispose();
+        }
+
+        highlightLifetimes.Clear();
+
+        foreach (var cellDisplayControl in controls)
+        {
+            var highlightLifetime = new Lifetime();
+            highlightLifetimes.Add(highlightLifetime);
+            if (cellDisplayControl is Label)
+            {
+                var label = (cellDisplayControl as Label);
+                var originalText = label.Text;
+                label.Text = label.Text.ToBlack().ToDifferentBackground(HasFocus ? RGB.Cyan : RGB.DarkGray);
+                highlightLifetime.OnDisposed(() =>
+                {
+                    if (label.IsExpired == false)
+                    {
+                        label.Text = originalText;
+                    }
+                });
+            }
+            else
+            {
+                var originalFg = cellDisplayControl.Foreground;
+                var originalBg = cellDisplayControl.Background;
+                cellDisplayControl.Foreground = RGB.White;
+                cellDisplayControl.Background = HasFocus ? RGB.Cyan : RGB.DarkGray;
+                highlightLifetime.OnDisposed(() =>
+                {
+                    if (cellDisplayControl.IsExpired == false)
+                    {
+                        cellDisplayControl.Foreground = originalFg;
+                        cellDisplayControl.Background = originalBg;
+                    }
+                });
+            }
+        }
+    }
+
+    private void FirstPageClicked()
+    {
+        topOfPageDataIndex = 0;
+        SelectedRowIndex = topOfPageDataIndex;
+    }
+
+    private void PreviousPageClicked()
+    {
+        topOfPageDataIndex = Math.Max(0, topOfPageDataIndex - presenter.MaxRowsThatCanBePresented);
+        SelectedRowIndex = topOfPageDataIndex;
+    }
+
+    private void NextPageClicked()
+    {
+        topOfPageDataIndex = Math.Min(options.DataSource.Count - 1, topOfPageDataIndex + presenter.MaxRowsThatCanBePresented);
+        SelectedRowIndex = topOfPageDataIndex;
+    }
+
+    private void LastPageClicked()
+    {
+        topOfPageDataIndex = (PageCount - 1) * presenter.MaxRowsThatCanBePresented;
+        SelectedRowIndex = topOfPageDataIndex;
+    }
+
+    private void HandleArrows(ConsoleKeyInfo keyInfo)
+    {
+        if (options.SelectionMode != ListViewerSelectionMode.None)
+        {
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                if (SelectedRowIndex > 0)
+                {
+                    if (SelectedRowIndex == topOfPageDataIndex)
+                    {
+                        topOfPageDataIndex = Math.Max(0, SelectedRowIndex - presenter.MaxRowsThatCanBePresented);
+
+                    }
+
+                    SelectedRowIndex--;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                if (SelectedRowIndex < options.DataSource.Count - 1)
+                {
+                    if (SelectedRowIndex == topOfPageDataIndex + presenter.MaxRowsThatCanBePresented - 1)
+                    {
+                        topOfPageDataIndex = SelectedRowIndex + 1;
+                    }
+                    SelectedRowIndex++;
+                }
+            }
+        }
+
+        if (options.SelectionMode == ListViewerSelectionMode.Cell)
+        {
+            if (keyInfo.Key == ConsoleKey.LeftArrow)
+            {
+                if (SelectedColumnIndex > 0)
+                {
+                    SelectedColumnIndex--;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.RightArrow)
+            {
+                if (SelectedColumnIndex < options.Columns.Count - 1)
+                {
+                    SelectedColumnIndex++;
+                }
+            }
+        }
+    }
+}
+
+
+internal class DataGridPresentationRow
+{
+    public List<Func<ConsoleControl>> Cells { get; set; } = new List<Func<ConsoleControl>>();
+}
+
+internal class PagerState
+{
+    public bool AllowRandomAccess { get; set; }
+    public bool CanGoBackwards { get; set; }
+    public bool CanGoForwards { get; set; }
+    public ConsoleString CurrentPageLabelValue { get; set; }
+}
+
+internal class ListViewerPanelOptions
+{
+    public bool ShowColumnHeaders { get; set; } = true;
+    public bool ShowPager { get; set; } = true;
+    public bool EnablePagerKeyboardShortcuts { get; set; } = true;
+    public List<HeaderDefinition> Columns { get; set; }
+    public List<DataGridPresentationRow> Rows { get; set; }
+    public PagerState PagerState { get; set; }
+}
+
+internal class ListViewerPanel : ProtectedConsolePanel
+{
+    private RandomAccessPager pager;
+    private GridLayout gridLayout;
+    private ConsolePanel pagerContainer;
+    private GridLayoutOptions layoutOptions;
+    private bool firstButtonFocused, previousButtonFocused, nextButtonFocused, lastButtonFocused;
+    public ListViewerPanelOptions Options { get; private set; }
+    public Dictionary<int, List<ConsoleControl>> ControlsByRow { get; private set; } = new Dictionary<int, List<ConsoleControl>>();
+    public Event FirstPageClicked { get; private set; } = new Event();
+    public Event PreviousPageClicked { get; private set; } = new Event();
+    public Event NextPageClicked { get; private set; } = new Event();
+    public Event LastPageClicked { get; private set; } = new Event();
+    public Event BeforeRecompose { get; private set; } = new Event();
+    public Event AfterRecompose { get; private set; } = new Event();
+    public int MaxRowsThatCanBePresented => Options.ShowColumnHeaders ? Math.Max(0, Height - 2) : Math.Max(0, Height - 1);
+
+    public ListViewerPanel(ListViewerPanelOptions options)
+    {
+        this.Options = options;
+        layoutOptions = new GridLayoutOptions() { Columns = options.Columns.Cast<GridColumnDefinition>().ToList(), Rows = new List<GridRowDefinition>() };
+        Subscribe(nameof(Bounds), Recompose, this);
+    }
+
+    public void Recompose()
+    {
+        if (MaxRowsThatCanBePresented == 0) return;
+        BeforeRecompose.Fire();
+        SnapshotPagerFocus();
+        DisposeGrid();
+        ComposeGridLayout();
+        ComposeDataCells();
+        ComposePager();
+        AfterRecompose.Fire();
+    }
+
+    private void SnapshotPagerFocus()
+    {
+        firstButtonFocused = pager != null && pager.FirstPageButton.HasFocus;
+        previousButtonFocused = pager != null && pager.PreviousPageButton.HasFocus;
+        nextButtonFocused = pager != null && pager.NextPageButton.HasFocus;
+        lastButtonFocused = pager != null && pager.LastPageButton.HasFocus;
+    }
+
+    private void DisposeGrid()
+    {
+        gridLayout?.Dispose();
+        ControlsByRow.Clear();
+    }
+
+    private void ComposeGridLayout()
+    {
+        layoutOptions.Rows = new List<GridRowDefinition>();
+        for (var i = 0; i < Height; i++)
+        {
+            layoutOptions.Rows.Add(new GridRowDefinition() { Height = 1, Type = GridValueType.Pixels });
+        }
+
+        gridLayout = ProtectedPanel.Add(new GridLayout(layoutOptions.GetRowSpec(), layoutOptions.GetColumnSpec())).Fill();
+    }
+    private void ComposeDataCells()
+    {
+        if (Options.ShowColumnHeaders)
+        {
+            for (var col = 0; col < Options.Columns.Count; col++)
+            {
+                gridLayout.Add(new Label() { Text = Options.Columns[col].Header }, col, 0);
+            }
+        }
+
+        var dataRowStartIndex = Options.ShowColumnHeaders ? 1 : 0;
+        var currentIndex = 0;
+        for (var gridLayoutRow = dataRowStartIndex; gridLayoutRow < dataRowStartIndex + MaxRowsThatCanBePresented; gridLayoutRow++)
+        {
+            if (currentIndex >= Options.Rows.Count) break;
+            var dataItem = Options.Rows[currentIndex];
+            var rowControls = new List<ConsoleControl>();
+            ControlsByRow.Add(currentIndex, rowControls);
+            for (var gridLayoutCol = 0; gridLayoutCol < Options.Columns.Count; gridLayoutCol++)
+            {
+                var columnDefinition = Options.Columns[gridLayoutCol];
+                var cellDisplayControl = gridLayout.Add(dataItem.Cells[gridLayoutCol].Invoke(), gridLayoutCol, gridLayoutRow);
+                rowControls.Add(cellDisplayControl);
+
+            }
+            currentIndex++;
+        }
+    }
+
+    private void ComposePager()
+    {
+        pagerContainer = gridLayout.Add(new ConsolePanel(), 0, Height - 1, layoutOptions.Columns.Count, 1);
+        pager = pagerContainer.Add(new RandomAccessPager(Options.EnablePagerKeyboardShortcuts)).CenterHorizontally();
+        pager.IsVisible = Options.ShowPager;
+        pager.FirstPageButton.Pressed.Subscribe(FirstPageClicked.Fire, pager);
+        pager.PreviousPageButton.Pressed.Subscribe(PreviousPageClicked.Fire, pager);
+        pager.NextPageButton.Pressed.Subscribe(NextPageClicked.Fire, pager);
+        pager.LastPageButton.Pressed.Subscribe(LastPageClicked.Fire, pager);
+        pager.FirstPageButton.CanFocus = Options.PagerState.CanGoBackwards;
+        pager.PreviousPageButton.CanFocus = Options.PagerState.CanGoBackwards;
+        pager.NextPageButton.CanFocus = Options.PagerState.CanGoForwards;
+        pager.LastPageButton.CanFocus = Options.PagerState.CanGoForwards;
+        pager.CurrentPageLabel.Text = Options.PagerState.CurrentPageLabelValue;
+        if (Options.PagerState.AllowRandomAccess == false)
+        {
+            pager.Controls.Remove(pager.LastPageButton);
+        }
+
+        if (firstButtonFocused) pager.FirstPageButton.Focus();
+        else if (previousButtonFocused) pager.PreviousPageButton.Focus();
+        else if (nextButtonFocused) pager.NextPageButton.Focus();
+        else if (lastButtonFocused) pager.LastPageButton.Focus();
+    }
+
+    private class RandomAccessPager : StackPanel
+    {
+        public Button FirstPageButton { get; private set; }
+        public Button PreviousPageButton { get; private set; }
+        public Label CurrentPageLabel { get; private set; }
+        public Button NextPageButton { get; private set; }
+        public Button LastPageButton { get; private set; }
+
+        public RandomAccessPager(bool enableShortcuts)
+        {
+            AutoSize = AutoSizeMode.Both;
+            Margin = 2;
+            Orientation = Orientation.Horizontal;
+            FirstPageButton = Add(new Button() { Text = "<<".ToConsoleString() });
+            PreviousPageButton = Add(new Button() { Text = "<".ToConsoleString() });
+            CurrentPageLabel = Add(new Label() { Text = "Page 1 of 1".ToConsoleString() });
+            NextPageButton = Add(new Button() { Text = ">".ToConsoleString() });
+            LastPageButton = Add(new Button() { Text = ">>".ToConsoleString() });
+
+            if (enableShortcuts)
+            {
+                FirstPageButton.Shortcut = new KeyboardShortcut(ConsoleKey.Home);
+                PreviousPageButton.Shortcut = new KeyboardShortcut(ConsoleKey.PageUp);
+                NextPageButton.Shortcut = new KeyboardShortcut(ConsoleKey.PageDown);
+                LastPageButton.Shortcut = new KeyboardShortcut(ConsoleKey.End);
+            }
+        }
+    }
+}
---
+++ src/klooie/tests/Controls/ListViewerTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace klooie.tests;
+[TestClass]
+[TestCategory(Categories.ConsoleApp)]
+public class ListViewerTests
+{
+    public class Item
+    {
+        public string Foo { get; set; }
+        public string Bar { get; set; }
+    }
+
+    public TestContext TestContext { get; set; }
+
+    [TestMethod]
+    public void ListViewer_Refresh() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        context.SecondsBetweenKeyframes = .05;
+        var items = Enumerable.Range(0, 100).Select(i => new Item() { Bar = "Bar" + i, Foo = "Foo" + i }).ToList();
+        var dataGrid = new ListViewer<Item>(new ListViewerOptions<Item>()
+        {
+            DataSource = items,
+            Columns = new List<HeaderDefinition<Item>>()
+            {
+                new HeaderDefinition<Item>()
+                {
+                    Header = "Foo".ToGreen(),
+                    Width = 20,
+                    Type = GridValueType.Pixels,
+                    Formatter = (item) => new Label(){ Text = item.Foo.ToConsoleString() }
+                },
+                new HeaderDefinition<Item>()
+                {
+                    Header = "Bar".ToRed(),
+                    Width = 1,
+                    Type = GridValueType.RemainderValue,
+                    Formatter = (item) => new Label(){ Text = item.Bar.ToConsoleString() }
+                }
+            }
+        });
+
+        var selectionLabel = ConsoleApp.Current.LayoutRoot.Add(new Label() { Text = "DEFAULT".ToConsoleString(), Height = 1 }).CenterHorizontally();
+        selectionLabel.Text = $"SelectedRowIndex: {dataGrid.SelectedRowIndex}, SelectedCellIndex: {dataGrid.SelectedColumnIndex}".ToConsoleString();
+        dataGrid.SelectionChanged.Subscribe(() => selectionLabel.Text = $"SelectedRowIndex: {dataGrid.SelectedRowIndex}, SelectedCellIndex: {dataGrid.SelectedColumnIndex}".ToConsoleString(), dataGrid);
+        ConsoleApp.Current.LayoutRoot.Add(dataGrid).Fill(padding: new Thickness(0, 0, 1, 0));
+        await context.PaintAndRecordKeyFrameAsync();
+
+        for (var i = 0; i < 100; i++)
+        {
+            items[i].Foo = "FooRefreshed" + i;
+            items[i].Bar = "BarRefreshed" + i;
+        }
+
+        dataGrid.Refresh();
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+    });
+
+    [TestMethod]
+    public void ListViewer_ProgrammaticSelection() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        var items = Enumerable.Range(0, 100).Select(i => new Item() { Bar = "Bar" + i, Foo = "Foo" + i }).ToList();
+
+        var dataGrid = new ListViewer<Item>(new ListViewerOptions<Item>()
+        {
+            DataSource = items,
+            Columns = new List<HeaderDefinition<Item>>()
+            {
+                new HeaderDefinition<Item>()
+                {
+                    Header = "Foo".ToGreen(),
+                    Width = 20,
+                    Type = GridValueType.Pixels,
+                    Formatter = (item) => new Label(){ Text = item.Foo.ToConsoleString() }
+                },
+                new HeaderDefinition<Item>()
+                {
+                    Header = "Bar".ToRed(),
+                    Width = 20,
+                    Type = GridValueType.Pixels,
+                    Formatter = (item) => new Label(){ Text = item.Bar.ToConsoleString() }
+                }
+            },
+        });
+
+        var selectionLabel = ConsoleApp.Current.LayoutRoot.Add(new Label() { Text = "DEFAULT".ToConsoleString(), Height = 1 }).CenterHorizontally();
+        selectionLabel.Text = $"SelectedRowIndex: {dataGrid.SelectedRowIndex}, SelectedCellIndex: {dataGrid.SelectedColumnIndex}".ToConsoleString();
+        dataGrid.SelectionChanged.Subscribe(() =>
+        {
+            selectionLabel.Text = $"SelectedRowIndex: {dataGrid.SelectedRowIndex}, SelectedCellIndex: {dataGrid.SelectedColumnIndex}".ToConsoleString();
+        }, dataGrid);
+        ConsoleApp.Current.LayoutRoot.Add(dataGrid).Fill(padding: new Thickness(0, 0, 1, 0));
+        await context.PaintAndRecordKeyFrameAsync();
+        dataGrid.SelectedRowIndex = 1;
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+
+    });
+
+    [TestMethod]
+    public void ListViewer_CellSelection() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        var items = Enumerable.Range(0, 100).Select(i => new Item() { Bar = "Bar" + i, Foo = "Foo" + i }).ToList();
+
+        var dataGrid = new ListViewer<Item>(new ListViewerOptions<Item>()
+        {
+            DataSource = items,
+            SelectionMode = ListViewerSelectionMode.Cell,
+            Columns = new List<HeaderDefinition<Item>>()
+                {
+                    new HeaderDefinition<Item>()
+                    {
+                        Header = "Foo".ToGreen(),
+                        Width = 20,
+                        Type = GridValueType.Pixels,
+                        Formatter = (item) => new Label(){ Text = item.Foo.ToConsoleString() }
+                    },
+                    new HeaderDefinition<Item>()
+                    {
+                        Header = "Bar".ToRed(),
+                        Width = 20,
+                        Type = GridValueType.Pixels,
+                        Formatter = (item) => new Label(){ Text = item.Bar.ToConsoleString() }
+                    }
+                }
+        });
+
+        
+        var selectionLabel = ConsoleApp.Current.LayoutRoot.Add(new Label() { Text = "DEFAULT".ToConsoleString(), Height = 1 }).CenterHorizontally();
+        selectionLabel.Text = $"SelectedRowIndex: {dataGrid.SelectedRowIndex}, SelectedCellIndex: {dataGrid.SelectedColumnIndex}".ToConsoleString();
+        dataGrid.SelectionChanged.Subscribe(() =>
+        {
+            selectionLabel.Text = $"SelectedRowIndex: {dataGrid.SelectedRowIndex}, SelectedCellIndex: {dataGrid.SelectedColumnIndex}".ToConsoleString();
+        }, dataGrid);
+        ConsoleApp.Current.LayoutRoot.Add(dataGrid).Fill(padding: new Thickness(0, 0, 1, 0));
+        dataGrid.Focus();
+        await context.PaintAndRecordKeyFrameAsync();
+
+        await ConsoleApp.Current.SendKey(new ConsoleKeyInfo(' ', ConsoleKey.DownArrow, false, false, false));
+        await context.PaintAndRecordKeyFrameAsync();
+
+
+        await ConsoleApp.Current.SendKey(new ConsoleKeyInfo(' ', ConsoleKey.RightArrow, false, false, false));
+        await context.PaintAndRecordKeyFrameAsync();
+
+        await ConsoleApp.Current.SendKey(new ConsoleKeyInfo(' ', ConsoleKey.DownArrow, false, false, false));
+        await context.PaintAndRecordKeyFrameAsync();
+
+        await ConsoleApp.Current.SendKey(new ConsoleKeyInfo(' ', ConsoleKey.LeftArrow, false, false, false));
+        await context.PaintAndRecordKeyFrameAsync();
+
+        ConsoleApp.Current.Stop();
+    });
+}
