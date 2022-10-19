56bf95ece99206972e666de6d94fdc15476fc583
Adam Abdelhamed <adamabdelhamed@live.com>
2022-10-19 10:51:11 -0400

    Migrate Divider

---
+++ src/klooie/klooie/Controls/Divider.cs
@@ -0,0 +1,40 @@
+﻿namespace klooie;
+
+/// <summary>
+/// A control that can be used to divide panels
+/// </summary>
+public class Divider : ConsoleControl
+{
+    public Orientation Orientation { get; private init; }
+
+    /// <summary>
+    /// Creates a divider
+    /// </summary>
+    public Divider(Orientation orientation)
+    {
+        CanFocus = false;
+        this.Orientation = orientation;
+
+        var limitedPropName = Orientation == Orientation.Vertical ? nameof(Width) : nameof(Height);
+        Func<int> limitedPropValue = Orientation == Orientation.Vertical ? () => Width : () => Height;
+        Func<int> limitedPropSetter = Orientation == Orientation.Vertical ? () => Width = 1 : () => Height = 1;
+        limitedPropSetter();
+        Subscribe(nameof(Bounds), () => ThrowArgumentExceptionIf(limitedPropName, limitedPropValue() != 1, $"{limitedPropName} must equal 1 for a {Orientation} divider"), this);
+    }
+    
+    private void ThrowArgumentExceptionIf(string propertyName, bool condition, string message)
+    {
+        if (condition == false) return;
+        throw new ArgumentException(message, propertyName);
+    }
+
+    /// <summary>
+    /// Paints the divider
+    /// </summary>
+    /// <param name="context">the bitmap target</param>
+    protected override void OnPaint(ConsoleBitmap context)
+    {
+        var c = Orientation == Orientation.Vertical ? '|' : '-';
+        context.Fill(new ConsoleCharacter(c, HasFocus ? FocusContrastColor : Foreground, HasFocus ? FocusColor : Background));
+    }
+}
---
+++ src/klooie/tests/Controls/DividerTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerArgs;
+using System;
+
+namespace klooie.tests;
+
+[TestClass]
+[TestCategory(Categories.ConsoleApp)]
+public class DividerTests
+{
+    public TestContext TestContext { get; set; }
+
+    
+    [TestMethod]
+    public void Divider_Horizontal() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        var divider = ConsoleApp.Current.LayoutRoot.Add(new Divider(Orientation.Horizontal)).FillHorizontally().DockToTop(padding: 5);
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+    });
+
+    [TestMethod]
+    public void Divider_Vertical() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        var divider = ConsoleApp.Current.LayoutRoot.Add(new Divider(Orientation.Vertical)).FillVertically().DockToLeft(padding: 5);
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+    });
+
+    [TestMethod]
+    public void Divider_HorizontalWithColorAndFocus() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        var divider = ConsoleApp.Current.LayoutRoot.Add(new Divider(Orientation.Horizontal) { Foreground = RGB.Green }).FillHorizontally().DockToTop(padding: 5);
+        await context.PaintAndRecordKeyFrameAsync();
+        divider.CanFocus = true;
+        divider.Focus();
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+    });
+
+    [TestMethod]
+    public void Divider_VerticalWithColorAndFocus() => AppTest.Run(TestContext.TestId(), UITestMode.KeyFramesVerified, async (context) =>
+    {
+        var divider = ConsoleApp.Current.LayoutRoot.Add(new Divider(Orientation.Vertical) { Foreground = RGB.Green }).FillVertically().DockToLeft(padding: 5);
+        await context.PaintAndRecordKeyFrameAsync();
+        await context.PaintAndRecordKeyFrameAsync();
+        divider.CanFocus = true;
+        divider.Focus();
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+    });
+
+    [TestMethod]
+    public void Divider_HorizontalPreventsInvalidSize() => AppTest.Run(TestContext.TestId(), UITestMode.Headless, async (context) =>
+    {
+        try
+        {
+            var divider = ConsoleApp.Current.LayoutRoot.Add(new Divider(Orientation.Horizontal)).FillVertically();
+            Assert.Fail("An exception should have been thrown");
+        }
+        catch(ArgumentException ex)
+        {
+            Assert.IsTrue(ex.Message.Contains(nameof(Divider.Height)));
+        }
+        ConsoleApp.Current.Stop();
+    });
+
+    [TestMethod]
+    public void Divider_VerticalPreventsInvalidSize() => AppTest.Run(TestContext.TestId(), UITestMode.Headless, async (context) =>
+    {
+        try
+        {
+            var divider = ConsoleApp.Current.LayoutRoot.Add(new Divider(Orientation.Vertical)).FillHorizontally();
+            Assert.Fail("An exception should have been thrown");
+        }
+        catch (ArgumentException ex)
+        {
+            Assert.IsTrue(ex.Message.Contains(nameof(Divider.Width)));
+        }
+        ConsoleApp.Current.Stop();
+    });
+}
