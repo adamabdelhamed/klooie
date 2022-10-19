f7cfb1d5370295c602ebb0af4a9255ea2de4247d
Adam Abdelhamed <adamabdelhamed@live.com>
2022-10-19 17:18:14 -0400

    Migrated ToggleControl

---
+++ src/klooie/klooie/Controls/ToggleControl.cs
@@ -0,0 +1,81 @@
+﻿namespace klooie;
+
+/// <summary>
+/// A control that toggles between on and off
+/// </summary>
+public class ToggleControl : ProtectedConsolePanel
+{
+    private Label valueLabel;
+    private Lifetime valueLifetime;
+    /// <summary>
+    /// Gets or sets the current On / Off value
+    /// </summary>
+    public bool On { get => Get<bool>(); set => Set(value); }
+
+    /// <summary>
+    /// The On label text
+    /// </summary>
+    public string OnLabel { get; set; } = " On  ";
+
+    /// <summary>
+    /// The Off label text
+    /// </summary>
+    public string OffLabel { get; set; } = " Off ";
+
+    /// <summary>
+    /// The On color
+    /// </summary>
+    public RGB OnColor { get => Get<RGB>(); set => Set(value); }
+
+    /// <summary>
+    /// The Off color
+    /// </summary>
+    public RGB OffColor { get => Get<RGB>(); set => Set(value); }
+
+    /// <summary>
+    /// Creates a new ToggleControl
+    /// </summary>
+    public ToggleControl()
+    {
+        CanFocus = true;
+        Width = 10;
+        Height = 1;
+        valueLabel = ProtectedPanel.Add(new Label());
+        Sync(nameof(On), async () => await Update(125), this);
+        Sync(nameof(IsVisible), async () => await Update(0), this);
+        Focused.Subscribe(async () => await Update(0), this);
+        Unfocused.Subscribe(async () => await Update(0), this);
+        KeyInputReceived.Subscribe(k => On = k.Key == ConsoleKey.Enter ? !On : On, this);
+        Ready.SubscribeOnce(async () => await Update(0));
+        OnColor = RGB.Magenta;
+        OffColor = RGB.Gray;
+        Foreground = RGB.Black;
+        Background = RGB.DarkGray;
+    }
+
+    private async Task Update(float duration)
+    {
+        valueLifetime?.TryDispose();
+        valueLifetime = new Lifetime();
+        var newLeft = On ? Width - valueLabel.Width : 0;
+        var newLabelBg = HasFocus ? FocusColor : On ? OnColor : OffColor;
+        var newFg = HasFocus ? FocusContrastColor : Foreground;
+        valueLabel.Text = On ? OnLabel.ToConsoleString(newFg, newLabelBg) : OffLabel.ToConsoleString(newFg, newLabelBg);
+
+        if (Application == null)
+        {
+            valueLabel.X = newLeft;
+            ProtectedPanel.Background = Background;
+            return;
+        }
+
+        var dest = new RectF(newLeft, valueLabel.Top, valueLabel.Bounds.Width, valueLabel.Bounds.Height);
+        await valueLabel.AnimateAsync(new ConsoleControlAnimationOptions()
+        {
+            IsCancelled = () => valueLifetime.IsExpired,
+            Duration = duration,
+            EasingFunction = EasingFunctions.EaseOutSoft,
+            Destination = () => dest,
+        });
+    }
+}
---
+++ src/klooie/tests/Controls/ToggleControlTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace klooie.tests;
+
+[TestClass]
+[TestCategory(Categories.ConsoleApp)]
+public class ToggleControlTests
+{
+    public TestContext TestContext { get; set; }
+
+    [TestMethod]
+    public void ToggleControl_Basic() => AppTest.RunCustomSize(TestContext.TestId(), UITestMode.KeyFramesVerified,15,3, async (context) =>
+    {
+        var toggle = ConsoleApp.Current.LayoutRoot.Add(new ToggleControl()).CenterBoth();
+        await context.PaintAndRecordKeyFrameAsync();
+        
+        toggle.On = true;
+        await Task.Delay(250);
+        await context.PaintAndRecordKeyFrameAsync();
+
+        toggle.On = false;
+        await Task.Delay(250);
+        await context.PaintAndRecordKeyFrameAsync();
+
+        toggle.Focus();
+        await Task.Delay(250);
+        await context.PaintAndRecordKeyFrameAsync();
+        ConsoleApp.Current.Stop();
+    });
+}
