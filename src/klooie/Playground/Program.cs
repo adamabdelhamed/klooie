98e0064361a41193c5dfcc739ea5bcd2c7156f5c
Adam Abdelhamed <adamabdelhamed@live.com>
2022-10-19 11:40:05 -0400

    Migrate XYChart

--- src/klooie/Playground/Program.cs
+++ src/klooie/Playground/Program.cs
@@ -1,3 +1,34 @@
-﻿using klooie.tests;
+﻿using klooie;
+using PowerArgs;
 
-new NavigateTests().NavigateCamera(false);
+var app = new ConsoleApp();
+
+app.Invoke(async () =>
+{
+    var options = new XYChartOptions()
+    {
+        Title = "Some fricken chart".ToOrange(),
+        Data = new List<Series>()
+        {
+            new Series()
+            {
+                Title = "Series1",
+                Points = Enumerable.Range(0,100).Select(x => new DataPoint(){ X = x, Y = x*x }).ToList(),
+                AllowInteractivity = true,
+                PlotCharacter = new ConsoleCharacter('X',RGB.Red),
+                PlotMode = PlotMode.Points
+            },
+             new Series()
+            {
+                Title = "Series2",
+                Points = Enumerable.Range(0,100).Select(x => new DataPoint(){ X = x, Y = 100 * x }).ToList(),
+                AllowInteractivity = true,
+                PlotCharacter = new ConsoleCharacter('O',RGB.Green),
+                PlotMode = PlotMode.Points
+            }
+        }
+    };
+    var chart = app.LayoutRoot.Add(new XYChart(options)).Fill();
+});
+
+app.Run();
