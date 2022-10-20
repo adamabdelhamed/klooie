995bdb49ce64c2532c8604d144a361c11c466656
Adam Abdelhamed <adamabdelhamed@live.com>
2022-10-20 13:08:08 -0400

    sample

--- src/klooie/Samples/GettingStarted/HelloWorld.cs
+++ src/klooie/Samples/GettingStarted/HelloWorld.cs
@@ -6,8 +6,8 @@ namespace klooie.Samples;
 // Define your application
 public class HelloWorld : ConsoleApp
 {
-    protected override Task Startup() =>
-        LayoutRoot.Add(new Label("Hello World! Press escape to exit.".ToOrange()))
+    protected override Task Startup() => LayoutRoot
+        .Add(new Label("Hello World! Press escape to exit.".ToOrange()))
         .CenterBoth()
         .FadeIn(2000);
 }
