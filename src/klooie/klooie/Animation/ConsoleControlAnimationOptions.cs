089f068c38d732ece8d6caa4d2e5ed4a25a5ec1e
Adam Abdelhamed <adamabdelhamed@live.com>
2022-10-24 13:10:07 -0400

    fix terrible bug

--- src/klooie/klooie/Animation/ConsoleControlAnimationOptions.cs
+++ src/klooie/klooie/Animation/ConsoleControlAnimationOptions.cs
@@ -3,6 +3,6 @@
 public sealed class ConsoleControlAnimationOptions : CommonAnimationOptions
 {
     public Func<RectF> Destination { get; set; }
-    public void Setter(ConsoleControl target, in RectF bounds) => target.Bounds = bounds.Round();
+    public void Setter(ConsoleControl target, in RectF bounds) => target.Bounds = bounds;
 }
 
