95bfba2d3ae4c04a23b74d734097bd1959db2eaa
Adam Abdelhamed <adamabdelhamed@live.com>
2025-05-23 11:12:02 -0400

    RecycleableStruct helper

---
+++ src/klooie/Observability/RecyclableStruct.cs
@@ -0,0 +1,16 @@
+﻿namespace klooie;
+public sealed class RecyclableStruct<T> : Recyclable where T : struct
+{
+    public T Value { get; set; }
+    private RecyclableStruct() { }
+    public static RecyclableStruct<T> Create() => RecyclableStructPool.Instance.Rent();
+    protected override void OnReturn() => Value = default;
+
+    private sealed class RecyclableStructPool : RecycleablePool<RecyclableStruct<T>>
+    {
+        private static readonly RecyclableStructPool _instance = new RecyclableStructPool();
+        public static RecyclableStructPool Instance => _instance;
+        private RecyclableStructPool() { }
+        public override RecyclableStruct<T> Factory() => new RecyclableStruct<T>();
+    }
+}
