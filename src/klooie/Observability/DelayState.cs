fc9f48ca820b36e751e6280232592060556a8820
Adam Abdelhamed <adamabdelhamed@live.com>
2025-05-28 14:54:24 -0400

    Add DelayState, which facilitates a pattern where callers check their dependencies before performing their work after a delay.

---
+++ src/klooie/Observability/DelayState.cs
@@ -0,0 +1,66 @@
+﻿namespace klooie;
+
+public class DelayState : Recyclable
+{
+    private RecyclableList<ILifetime> Dependencies;
+    private RecyclableList<int> Leases;
+
+    public ILifetime MainDependency => Dependencies.Items[0];
+
+    public bool IsStillValid
+    {
+        get
+        {
+            for (int i = 0; i < Leases.Count; i++)
+            {
+                if (Dependencies[i].IsStillValid(Leases[i]) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static DelayState Create(ILifetime dependency)
+    {
+        var ret = DelayStatePool.Instance.Rent();
+        ret.Dependencies = RecyclableListPool<ILifetime>.Instance.Rent(1);
+        ret.Leases = RecyclableListPool<int>.Instance.Rent(1);
+        ret.Dependencies.Items.Add(dependency);
+        ret.Leases.Items.Add(dependency.Lease);
+        return ret;
+    }
+
+    public void AddDependency(ILifetime dependency)
+    {
+        if (Dependencies == null)
+        {
+            Dependencies = RecyclableListPool<ILifetime>.Instance.Rent();
+            Leases = RecyclableListPool<int>.Instance.Rent();
+        }
+        Dependencies.Items.Add(dependency);
+        Leases.Items.Add(dependency.Lease);
+    }
+
+    public static DelayState Create(RecyclableList<ILifetime> dependencies)
+    {
+        var ret = DelayStatePool.Instance.Rent();
+        ret.Dependencies = dependencies;
+        ret.Leases = RecyclableListPool<int>.Instance.Rent(dependencies.Count);
+        for (int i = 0; i < dependencies.Count; i++)
+        {
+            ret.Leases.Items.Add(dependencies[i].Lease);
+        }
+        return ret;
+    }
+
+    protected override void OnReturn()
+    {
+        base.OnReturn();
+        Dependencies?.Dispose();
+        Dependencies = null;
+        Leases?.Dispose();
+        Leases = null;
+    }
+}
