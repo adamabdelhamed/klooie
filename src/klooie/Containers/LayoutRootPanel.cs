52aa8d7b8b25b93a3fffa9fb1a774d4781ffa120
Adam Abdelhamed <adamabdelhamed@live.com>
2025-06-07 14:46:51 -0400

    bump frame rate

--- src/klooie/Containers/LayoutRootPanel.cs
+++ src/klooie/Containers/LayoutRootPanel.cs
@@ -29,7 +29,7 @@ public partial class LayoutRootPanel : ConsolePanel
         lastConsoleHeight = ConsoleProvider.Current.WindowHeight - 1;
         ResizeTo(lastConsoleWidth, lastConsoleHeight);
         ConsoleApp.Current!.EndOfCycle.Subscribe(DebounceResize, this);
-        ConsoleApp.Current.EndOfCycle.SubscribeThrottled(DrainPaints, this, 33);
+        ConsoleApp.Current.EndOfCycle.SubscribeThrottled(DrainPaints, this, 70);
         DescendentAdded.Subscribe(OnDescendentAdded, this);
         OnDisposed(RestoreConsoleState);
         FocusStackDepth = 1;
