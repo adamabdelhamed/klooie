283ee4d31c1bf4beddf23b4f3e086aaab70fdc50
Adam Abdelhamed <adamabdelhamed@live.com>
2025-07-14 12:33:28 -0400

    Make seek increment based on the current zoom level so when you are zoomed out the increment is larger and when you are zoomed in the increment is smaller.

--- src/klooie/Audio/DAW/TimelineInputModes/SeekMode.cs
+++ src/klooie/Audio/DAW/TimelineInputModes/SeekMode.cs
@@ -12,11 +12,12 @@ public class SeekMode : TimelineInputMode
     {
         if (k.Modifiers.HasFlag(ConsoleModifiers.Alt) || k.Modifiers.HasFlag(ConsoleModifiers.Control) || k.Modifiers.HasFlag(ConsoleModifiers.Shift)) return;
         var Player = Timeline.Player;
-        // Arrow keys move playhead, not the viewport.
-        if (k.Key == ConsoleKey.LeftArrow || k.Key == ConsoleKey.A) Player.SeekBy(-1);
-        else if (k.Key == ConsoleKey.RightArrow || k.Key == ConsoleKey.D) Player.SeekBy(1);
-        else if (k.Key == ConsoleKey.UpArrow || k.Key == ConsoleKey.W) Player.SeekBy(-0.25); // Fine-tune
-        else if (k.Key == ConsoleKey.DownArrow || k.Key == ConsoleKey.S) Player.SeekBy(+0.25); // Fine-tune
+        if (k.Key == ConsoleKey.LeftArrow || k.Key == ConsoleKey.A) Player.SeekBy(-Timeline.BeatsPerColumn);
+        else if (k.Key == ConsoleKey.RightArrow || k.Key == ConsoleKey.D) Player.SeekBy(Timeline.BeatsPerColumn);
+        else if (k.Key == ConsoleKey.UpArrow || k.Key == ConsoleKey.W) Player.SeekBy(-0.25 * Timeline.BeatsPerColumn); // Fine-tune
+        else if (k.Key == ConsoleKey.DownArrow || k.Key == ConsoleKey.S) Player.SeekBy(+0.25 * Timeline.BeatsPerColumn); // Fine-tune
+        else if (k.Key == ConsoleKey.Home) Player.Seek(0); // Go to start
+        else if (k.Key == ConsoleKey.End) Player.Seek(Timeline.NoteSource.Select(n => n.StartBeat + n.DurationBeats).LastOrDefault()); // Go to end
 
         else return;
         Timeline.RefreshVisibleSet();
