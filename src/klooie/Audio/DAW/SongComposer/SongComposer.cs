b7e4035e2f1c315fabd4f3d27e2ad9d473693178
Adam Abdelhamed <adamabdelhamed@live.com>
2025-08-11 09:11:09 -0400

    Close tweaker on escape key

--- src/klooie/Audio/DAW/SongComposer/SongComposer.cs
+++ src/klooie/Audio/DAW/SongComposer/SongComposer.cs
@@ -50,6 +50,7 @@ public class SongComposer : ProtectedConsolePanel
             if (MidiProvider.TryConnect(MidiProvider.GetProductNames()[0], out IMidiInput input) == false) return;
             var maxDepth = Math.Max(ConsoleApp.Current.LayoutRoot.FocusStackDepth, ConsoleApp.Current.LayoutRoot.Descendents.Max(d => d.FocusStackDepth));
             var tweaker = ConsoleApp.Current.LayoutRoot.Add(new SynthTweakerPanel(input, WorkspaceSession.Current.CurrentSong.BeatsPerMinute) { FocusStackDepth = maxDepth+1 }).Fill();
+            ConsoleApp.Current.PushKeyForLifetime(ConsoleKey.Escape, () => tweaker.Dispose(), tweaker);
         }, uiHint);
     }
 
