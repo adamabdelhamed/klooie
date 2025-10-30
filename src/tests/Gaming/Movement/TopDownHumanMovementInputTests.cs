563c58d7607017c43113ba31c7de5a0f6002857f
Adam Abdelhamed <adamabdelhamed@live.com>
2025-10-30 08:35:27 -0400

    Add missing test category

--- src/tests/Gaming/Movement/TopDownHumanMovementInputTests.cs
+++ src/tests/Gaming/Movement/TopDownHumanMovementInputTests.cs
@@ -7,6 +7,7 @@ using System;
 
 namespace tests.Gaming;
 [TestClass]
+[TestCategory(Categories.Gaming)]
 public class TopDownHumanMovementInputTests
 {
     public TestContext TestContext { get; set; }
