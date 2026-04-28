83945b2abe830c8b2990dd7ae61c7f6e92df33a0
adamabdelhamed <adamabdelhamed@live.com>
2026-04-28 06:41:10 -0400

    Iterate on compiled maps

--- src/klooie/Drawing/SystemDrawingBitmapConverter.cs
+++ src/klooie/Drawing/SystemDrawingBitmapConverter.cs
@@ -7,13 +7,16 @@ using System.Text;
 namespace klooie;
 public static class SystemDrawingBitmapConverter
 {
-    public static ConsoleBitmap Convert(Bitmap bitmap, int scale = 3, RGB? chromaKey = null)
+    public static ConsoleBitmap Convert(Bitmap bitmap, int scale = 3, RGB? chromaKey = null, ConsoleBitmap background = null, float alphaCoverageThreshold = 0, bool[,] alphaCoverageMask = null)
     {
         var sourceCellW = scale;
         var sourceCellH = sourceCellW * 2;
 
         var w = bitmap.Width / sourceCellW;
         var h = bitmap.Height / sourceCellH;
+
+        if (background != null && (background.Width != w || background.Height != h)) throw new ArgumentException("Background dimensions must match converted bitmap dimensions.", nameof(background));
+        if (alphaCoverageMask != null && (alphaCoverageMask.GetLength(0) != w || alphaCoverageMask.GetLength(1) != h)) throw new ArgumentException("Alpha coverage mask dimensions must match converted bitmap dimensions.", nameof(alphaCoverageMask));
 
         var consoleBitmap = ConsoleBitmap.Create(w, h);
 
@@ -46,18 +49,32 @@ public static class SystemDrawingBitmapConverter
                             for (var x = 0; x < sourceCellW; x++)
                             {
                                 var pixel = row + ((startX + x) * 4);
+                                var alpha = pixel[3];
 
-                                b += pixel[0];
-                                g += pixel[1];
-                                r += pixel[2];
-                                a += pixel[3];
+                                if (background == null || alpha == 255)
+                                {
+                                    b += pixel[0];
+                                    g += pixel[1];
+                                    r += pixel[2];
+                                }
+                                else
+                                {
+                                    var backgroundPixel = background.GetPixel(cx, cy).BackgroundColor;
+                                    b += Blend(pixel[0], backgroundPixel.B, alpha);
+                                    g += Blend(pixel[1], backgroundPixel.G, alpha);
+                                    r += Blend(pixel[2], backgroundPixel.R, alpha);
+                                }
+
+                                a += alpha;
                                 count++;
                             }
                         }
 
                         var avg = new RGB((byte)(r / count), (byte)(g / count), (byte)(b / count));
-                        var avgA = (byte)(a / count);
-                        if(avgA == 0)
+                        var alphaCoverage = a / (255f * count);
+                        var hasAlphaCoverage = alphaCoverageThreshold <= 0 ? alphaCoverage > 0 : alphaCoverage >= alphaCoverageThreshold;
+                        if (alphaCoverageMask != null) alphaCoverageMask[cx, cy] = hasAlphaCoverage;
+                        if(hasAlphaCoverage == false)
                         {
                             avg = chromaKey ?? ConsoleString.DefaultBackgroundColor;
                         }
@@ -73,4 +90,6 @@ public static class SystemDrawingBitmapConverter
         }
         return consoleBitmap;
     }
+
+    private static long Blend(byte source, byte background, byte alpha) => ((source * alpha) + (background * (255 - alpha))) / 255;
 }
