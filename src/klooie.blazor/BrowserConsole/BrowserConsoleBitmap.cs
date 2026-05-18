e0065375316aae17ca2d4f919f2fe341637f91e2
adamabdelhamed <adamabdelhamed@live.com>
2026-05-18 09:40:11 -0400

    blazer starter

---
+++ src/klooie.blazor/BrowserConsole/BrowserConsoleBitmap.cs
@@ -0,0 +1,52 @@
+namespace klooie.blazor.BrowserConsole;
+
+public sealed class BrowserConsoleBitmap
+{
+    private readonly BrowserConsoleCell[] cells;
+
+    public BrowserConsoleBitmap(int width, int height, BrowserConsoleCell? fill = null)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(width);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(height);
+
+        Width = width;
+        Height = height;
+        cells = Enumerable.Repeat(fill ?? BrowserConsoleCell.Empty, width * height).ToArray();
+    }
+
+    public int Width { get; }
+    public int Height { get; }
+    public IReadOnlyList<BrowserConsoleCell> Cells => cells;
+
+    public BrowserConsoleCell this[int x, int y]
+    {
+        get => cells[GetIndex(x, y)];
+        set => cells[GetIndex(x, y)] = value;
+    }
+
+    public void FillRect(int x, int y, int width, int height, BrowserConsoleCell cell)
+    {
+        for (var row = y; row < y + height; row++)
+        {
+            for (var column = x; column < x + width; column++)
+            {
+                this[column, row] = cell;
+            }
+        }
+    }
+
+    public void Write(int x, int y, string text, string foregroundColor, string backgroundColor)
+    {
+        for (var i = 0; i < text.Length && x + i < Width; i++)
+        {
+            this[x + i, y] = new BrowserConsoleCell(text[i].ToString(), foregroundColor, backgroundColor);
+        }
+    }
+
+    private int GetIndex(int x, int y)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual((uint)x, (uint)Width);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual((uint)y, (uint)Height);
+        return y * Width + x;
+    }
+}
